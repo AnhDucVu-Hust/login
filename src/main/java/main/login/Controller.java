d00df534b42de6bc6b9ae2ec22d90201cfa284ca
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2022-11-30 13:33:09 +0700

    commit

--- src/main/java/main/login/Controller.java
+++ src/main/java/main/login/Controller.java
@@ -7,8 +7,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-
-public class Controller{
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+public class Controller {
     @FXML
     private TextField txtUser;
     @FXML
@@ -18,17 +22,36 @@ public class Controller{
     @FXML
     private Button btnLogin;
     @FXML
-    protected void handleButtonAction(MouseEvent event){
+    protected void handleButtonAction(MouseEvent event) throws IOException {
         if (event.getSource() == btnLogin){
-            logIN();
+            if (logIN()=="Success") {
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                //stage.setMaximized(true);
+                stage.close();
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("home.fxml")));
+                stage.setScene(scene);
+                stage.show();
+            }
         }
     }
-    private void logIN(){
+    private String logIN(){
+        String status;
         String email=txtUser.getText().toString();
         String password=txtPass.getText().toString();
-        if (password.length() < 8) {
-            iblError.setText("Error");
+        if (email.isEmpty() || password.isEmpty()) {
+            iblError.setText("Empty credentials");
+            status="Error";
         }
+        else if (password.length() < 8){
+            status="Error";
+            iblError.setText("Enter correct username/password");
+        }
+        else {
+            iblError.setText("Login thành công...Xin đợi..");
+            status="Success";
+        }
+        return status;
     }
 
 }
